0a41d6d8b1692711625706c24ac692e0b0efa50a
Dan Vlasceanu <danvlasceanu2000@gmail.com>
2026-04-17 00:37:12 +0300

    Fix GitHub Pages build and base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PORR_Construct_Quiz-main/',
+  base: '/PORR_Construct_Quiz/',
   build: {
     outDir: 'dist',
   },
